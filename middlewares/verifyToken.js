82f18dc0d313547cfc31e1b0644ea73fb4954724
Eberechi Nwankudu <eberechinwankudu@gmail.com>
2024-06-01 18:03:51 +0100

    bug

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,18 +1,36 @@
-const jwt =require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const errorHandler  =require('./error');
+
 
 const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
 
-  if (!token) return next(errorHandler(401, 'Unauthorized'));
+  if (!token) {
+    return next(errorHandler(403, 'Access denied. No token provided.'));
+  }
 
-  jwt.verify(token, process.env.JWT_Token, (err, user) => {
-    if (err) return next(errorHandler(403, 'Forbidden'));
-
-    req.user = user;
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_TOKEN);
+    req.user = decoded;
     next();
-  });
+  } catch (ex) {
+    return next(errorHandler(401, 'Invalid token.'));
+  }
 };
+
+
+// const verifyToken = (req, res, next) => {
+//   const token = req.cookies.access_token;
+
+//   if (!token) return next(errorHandler(401, 'Unauthorized'));
+
+//   jwt.verify(token, process.env.JWT_Token, (err, user) => {
+//     if (err) return next(errorHandler(403, 'Forbidden'));
+
+//     req.user = user;
+//     next();
+//   });
+// };
 
 
 module.exports = verifyToken
